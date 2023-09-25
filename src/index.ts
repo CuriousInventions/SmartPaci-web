68dd2480ba20be5928d515427221a13282f23fd8
TequinDragon <github@tequ.in>
2023-09-26 03:39:17 +1300

    Migrate to Vite. Fetch version information via Bluetooth

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import {MCUManager} from "./mcumgr";
-import {Paci} from "./paci";
+// import {MCUManager} from "./mcumgr";
+import {CalibrationType, InputType, Paci} from "./paci";
 import 'bootstrap';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
 
 
-function onReady(event: Event)
+function onReady(_: Event)
 {
     // const mgr = new MCUManager();
     const paci = new Paci();
+    const idCalibrateSuckMinButton = document.getElementById("btnCalibrateSuckMin")!;
+    const idCalibrateSuckMaxButton = document.getElementById("btnCalibrateSuckMax")!;
     const idConnectButton = document.getElementById("btnDeviceConnect")!;
     const idDisconnectButton = document.getElementById("btnDeviceDisconnect")!;
     const idBiteProgress = document.getElementById("progBite")!;
@@ -17,26 +19,51 @@ function onReady(event: Event)
     const idSuck1Progress = document.getElementById("progSuck1")!;
     const idSuck2Progress = document.getElementById("progSuck2")!;
     const idSuck3Progress = document.getElementById("progSuck3")!;
+    const idVersionLabel = document.getElementById("lblVersion")!;
+    const idStatusLabel = document.getElementById("lblStatus")!;
 
-    paci.onBiteReceived(value => {
-        idBiteProgress.style.width = ((value / 255) * 100) + "%";
-    });
-    paci.onSuckReceived(values => {
-        idSuck0Progress.style.width = ((values[0] / 255) * 100) + "%";
-        idSuck1Progress.style.width = ((values[1] / 255) * 100) + "%";
-        idSuck2Progress.style.width = ((values[2] / 255) * 100) + "%";
-        idSuck3Progress.style.width = ((values[3] / 255) * 100) + "%";
+    idStatusLabel.innerText = "Disconnected";
+
+    paci.addEventListener("disconnected", () => {
+        idStatusLabel.innerText = "Disconnected";
     });
 
-    idConnectButton.addEventListener('click', async event => {
+    paci.addEventListener("bite", event => {
+        idBiteProgress.style.width = ((event.detail.value / 255) * 100) + "%";
+    });
+    paci.addEventListener("suck", event => {
+        idSuck0Progress.style.width = ((event.detail.values[0] / 255) * 100) + "%";
+        idSuck1Progress.style.width = ((event.detail.values[1] / 255) * 100) + "%";
+        idSuck2Progress.style.width = ((event.detail.values[2] / 255) * 100) + "%";
+        idSuck3Progress.style.width = ((event.detail.values[3] / 255) * 100) + "%";
+    });
+
+    idConnectButton.addEventListener('click', async _ => {
         await paci.connect();
+        idStatusLabel.innerText = "Connected";
+        let version = await paci.getFirmwareVersion();
+        idVersionLabel.innerText = version;
+        
+        await paci.getFirmwareCommit().then(commit => {
+            if (commit.length > 0) {
+                version += ` (${commit})`;
+                idVersionLabel.innerText = version;
+            }
+        });
     });
 
-    idDisconnectButton.addEventListener('click', async event => {
+    idDisconnectButton.addEventListener('click', async _ => {
         await paci.disconnect();
     });
     
-    console.log("Ready!", event);
+    idCalibrateSuckMinButton.addEventListener('click', async _ => {
+        await paci.calibrateInput(InputType.Suck, CalibrationType.Min);
+    });
+    
+    idCalibrateSuckMaxButton.addEventListener('click', async _ => {
+        await paci.calibrateInput(InputType.Suck, CalibrationType.Max);
+    });
+
 }
 
 window.addEventListener("DOMContentLoaded", onReady);
--- src/paci.ts
+++ src/paci.ts
@@ -1,23 +1,77 @@
+import { ControlRequest, CalibrateSensor, VersionRequest, ControlResponse, Version, CalibrateSensor_Sensor } from "./generated/bluetooth_pb";
+import {toHex} from "@smithy/util-hex-encoding";
 
-export class Paci {
-    readonly SERVICE_UUID                = "abbd1ef0-62e8-493b-8549-8cb891483e20";
+export enum InputType {
+    Bite,
+    Suck,
+}
+
+export enum CalibrationType {
+    Min,
+    Max,
+}
+
+export interface PaciVersion {
+    major: number;
+    minor: number;
+    build: number;
+    commit: string|null;
+    datetime: Date|null;
+    descript: string|null;
+}
+
+export interface PaciEventMap {
+    'bite': CustomEvent<{value: number}>;
+    'suck': CustomEvent<{values: number[]}>;
+    'disconnect': Event;
+    "firmwareVersion": CustomEvent<{version: PaciVersion}>;
+}
+
+// Helper interface to superimpose our custom events (and Event types) to the EventTarget
+// See: https://dev.to/43081j/strongly-typed-event-emitters-using-eventtarget-in-typescript-3658
+interface PaciEventTarget extends EventTarget {
+    addEventListener<K extends keyof PaciEventMap>(
+        type: K,
+        listener: (ev: PaciEventMap[K]) => void,
+        options?: boolean | AddEventListenerOptions
+    ): void;
+    addEventListener(
+        type: string,
+        callback: EventListenerOrEventListenerObject | null,
+        options?: EventListenerOptions | boolean
+    ): void;
+}
+
+// Again, see: https://dev.to/43081j/strongly-typed-event-emitters-using-eventtarget-in-typescript-3658
+const typedEventTarget = EventTarget as {new(): PaciEventTarget; prototype: PaciEventTarget};
+
+export class Paci extends typedEventTarget {
+    readonly SERVICE_UUID = "abbd1ef0-62e8-493b-8549-8cb891483e20";
     readonly CHARACTERISTIC_CONTROL_UUID = "abbd1ef1-62e8-493b-8549-8cb891483e20";
     readonly CHARACTERISTIC_FORCE_UUID   = "abbd1ef2-62e8-493b-8549-8cb891483e20";
     readonly CHARACTERISTIC_BITE_UUID    = "abbd1ef3-62e8-493b-8549-8cb891483e20";
-    
+
     private _device: BluetoothDevice | null;
     private _service: BluetoothRemoteGATTService | undefined;
+    private _controlCharacteristic: BluetoothRemoteGATTCharacteristic | undefined;
     private _biteCharacteristic: BluetoothRemoteGATTCharacteristic | undefined;
     private _suckCharacteristic: BluetoothRemoteGATTCharacteristic | undefined;
-    private _onBiteReceived: ((this: this, value: number) => any) | undefined;
-    private _onSuckReceived: ((this: this, values: number[]) => any) | undefined;
 
-    constructor(){
+    private _disconnectSignal: AbortController;
+
+    private _firmwareVersion: Promise<PaciVersion> | null;
+
+    constructor() {
+        super();
+
         this._device = null;
+        this._firmwareVersion = null;
+
+        this._disconnectSignal = new AbortController();
     }
 
     async connect(): Promise<void> {
-        let params : RequestDeviceOptions = {
+        let params: RequestDeviceOptions = {
             filters: [
                 {
                     services: [this.SERVICE_UUID],
@@ -26,31 +80,114 @@ export class Paci {
         };
 
         this._device = await navigator.bluetooth.requestDevice(params);
+        // Assign a new abort controller only after a new device becomes our subject.
+        // Abort signal used to clean up event listeners.
+        this._disconnectSignal = new AbortController();
+
+        this._device.addEventListener("gattserverdisconnected", 
+            () => this.dispatchEvent(new Event("disconnected")),
+            {signal: this._disconnectSignal.signal} as any);
+
         const server = await this._device.gatt?.connect();
         this._service = await server!.getPrimaryService(this.SERVICE_UUID);
-        this._biteCharacteristic = await this._service.getCharacteristic(this.CHARACTERISTIC_BITE_UUID);
-        this._suckCharacteristic = await this._service.getCharacteristic(this.CHARACTERISTIC_FORCE_UUID);
+        this._controlCharacteristic = await this._service.getCharacteristic(this.CHARACTERISTIC_CONTROL_UUID);
+        this._biteCharacteristic    = await this._service.getCharacteristic(this.CHARACTERISTIC_BITE_UUID);
+        this._suckCharacteristic    = await this._service.getCharacteristic(this.CHARACTERISTIC_FORCE_UUID);
 
-        this._biteCharacteristic.addEventListener('characteristicvaluechanged', async event => {
+        this._biteCharacteristic.addEventListener('characteristicvaluechanged', event => {
             const char = event.target as BluetoothRemoteGATTCharacteristic;
             const value = char.value?.getUint8(0) ?? 0;
-            if (this._onBiteReceived)
-                await this._onBiteReceived(value);
-        });
+            this.dispatchEvent(new CustomEvent("bite", {detail: {value}}));
+        }, {signal: this._disconnectSignal.signal} as any);
 
-        this._suckCharacteristic.addEventListener('characteristicvaluechanged', async event => {
+        this._suckCharacteristic.addEventListener('characteristicvaluechanged', event => {
             const char = event.target as BluetoothRemoteGATTCharacteristic;
-            const forces :number[] = [];
-            for(let i = 0; i < char.value!.byteLength; i++)
-            {
+            const forces: number[] = [];
+            for (let i = 0; i < char.value!.byteLength; i++) {
                 forces[i] = char.value?.getUint8(i) ?? 0;
             }
-            if (this._onSuckReceived)
-                await this._onSuckReceived(forces);
+
+            this.dispatchEvent(new CustomEvent("suck", {detail: {values: forces}}));
+        }, {signal: this._disconnectSignal.signal} as any);
+
+        this._controlCharacteristic.addEventListener('characteristicvaluechanged', event => {
+            const char = event.target as BluetoothRemoteGATTCharacteristic;
+            if (char.value === undefined) {
+                return;
+            }
+
+            const response = ControlResponse.fromBinary(new Uint8Array(char.value.buffer));
+            switch (response.response.case)
+            {
+                case "firmwareVersion":
+                    const version = response.response.value as Version;
+                    toHex(version.commit)
+                    this.dispatchEvent(new CustomEvent("firmwareVersion", {
+                        detail: {
+                            version: {
+                                major: version.major,
+                                minor: version.minor,
+                                build: version.build,
+                                commit: toHex(version.commit),
+                                descript: version.descript,
+                                datetime: new Date(),
+                            }
+                        }
+                    }));
+
+                    break;
+                default:
+                    console.log(`Unsupported response (${response.response.case})`, response);
+                    return;
+            }
         });
 
-        this._biteCharacteristic.startNotifications();
-        this._suckCharacteristic.startNotifications();
+        await this._biteCharacteristic.startNotifications();
+        await this._suckCharacteristic.startNotifications();
+        await this._controlCharacteristic.startNotifications();
+    }
+
+    async getFirmwareVersion(): Promise<string> {
+        if (this._firmwareVersion === null) {
+            this._firmwareVersion = this._getFirmwareVersion();
+        }
+
+        return await this._firmwareVersion.then(version => {
+            let result = `${version.major}.${version.minor}.${version.build}`;
+            if (version.descript != null && version.descript.length > 0) {
+                result += `-${version.descript}`;
+            }
+            return result;
+        });
+    }
+
+    async getFirmwareDate(): Promise<Date|null> {
+        if (this._firmwareVersion === null) {
+            this._firmwareVersion = this._getFirmwareVersion();
+        }
+
+        return await this._firmwareVersion.then(version => version.datetime);
+    }
+
+    async getFirmwareCommit(): Promise<string> {
+        if (this._firmwareVersion === null) {
+            this._firmwareVersion = this._getFirmwareVersion();
+        }
+
+        return await this._firmwareVersion.then(version => version.commit ?? "");
+    }
+
+    private async _getFirmwareVersion(): Promise<PaciVersion> {
+        const request = new ControlRequest();
+
+        const p = new Promise<PaciVersion>(resolve => {
+            this.addEventListener("firmwareVersion" , event => resolve(event.detail.version), {once: true} as any);
+        });
+
+        request.request = {case: "firmwareVersion", value: new VersionRequest()};
+        await this._controlCharacteristic!.writeValueWithResponse(request.toBinary());
+        
+        return await p;
     }
 
     async disconnect(): Promise<void> {
@@ -58,15 +195,36 @@ export class Paci {
         await this._device?.gatt?.disconnect();
     }
 
-    onBiteReceived(listener: (this: this, value: number) => any): void {
-        this._onBiteReceived = listener;
+    // addEventListener(type: string, listener: EventListenerOrEventListenerObject, useCapture?: boolean): void {
+
+    // }
+
+    private async _sendRequest(request: ControlRequest): Promise<void> {
+        await this._controlCharacteristic!.writeValueWithResponse(request.toBinary());
     }
 
-    onSuckReceived(listener: (this: this, values: number[]) => any): void {
-        this._onSuckReceived = listener;
-    }
+    async calibrateInput(input: InputType, calibrate: CalibrationType): Promise<void> {
+        const calibrateMsg = new CalibrateSensor();
+        switch (input) {
+            case InputType.Bite:
+                if (calibrate == CalibrationType.Min) {
+                    calibrateMsg.sensor = CalibrateSensor_Sensor.BITE_MIN;
+                } else if (calibrate == CalibrationType.Max) {
+                    calibrateMsg.sensor = CalibrateSensor_Sensor.BITE_MAX;
+                }
+                break;
+            case InputType.Suck:
+                if (calibrate == CalibrationType.Min) {
+                    calibrateMsg.sensor = CalibrateSensor_Sensor.SUCK_MIN;
+                } else if (calibrate == CalibrationType.Max) {
+                    calibrateMsg.sensor = CalibrateSensor_Sensor.SUCK_MAX;
+                }
+                break;
+        }
 
-    addEventListener(type: string, listener: EventListenerOrEventListenerObject, useCapture?: boolean): void {
+        const request = new ControlRequest();
+        request.request = {case: "calibrateSensor", value: calibrateMsg};
 
+        return await this._sendRequest(request);
     }
 }
