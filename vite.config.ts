e0c0097a56116541fc449a44ab1c2299aeebd2d7
TequinDragon <github@tequ.in>
2023-10-04 11:19:36 +1300

    Allow more unused variabled to exist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ export default {
     root: 'src/',
     build: {
         // outDir is relative to the root config above.
-        outDir: '../dist'
+        outDir: '../dist',
+        emptyOutDir: true,
     }
 }
